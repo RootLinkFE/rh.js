69b7520b63482d00fcea0b2aee2951f6a5fc2d24
giscafer <giscafer@outlook.com>
2021-05-18 18:22:47 +0800

    chore: 删除测试文件

--- packages/cli/bin/src/rh/apis/index.ts
+++
@@ -1,9 +0,0 @@
-import { Api as Common } from './Common/Api';
-import { Api as Project } from './Project/Api';
-
-const RhApi = {
-  Common: new Common(),
-  Project: new Project(),
-};
-
-export default RhApi;
