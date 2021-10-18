2dbf9cc4cc9735e29aa5b36f97ede1c72c45e75b
giscafer <giscafer@outlook.com>
2021-10-18 19:44:37 +0800

    chore: componets gh-pages scripts

--- packages/components/scripts/gh-pages-publish.js
+++ packages/components/scripts/gh-pages-publish.js
@@ -1,13 +1,12 @@
-const { cd, exec, echo, touch } = require("shelljs");
-const { readFileSync } = require("fs");
-const url = require("url");
-const { execSync } = require("child_process");
-const config = require("./config");
-// require('./copy-cname');
-require("./spa-gh-pages");
+const { cd, exec, echo, touch } = require('shelljs');
+const { readFileSync } = require('fs');
+const url = require('url');
+const { execSync } = require('child_process');
 
-let repoUrl = config.docGitUrl;
-let pkg = JSON.parse(readFileSync("package.json"));
+require('./spa-gh-pages');
+
+let repoUrl = 'https://github.com/RootLinkFE/roothub-site';
+let pkg = JSON.parse(readFileSync('package.json'));
 /* if (typeof pkg.repository === 'object') {
   if (!pkg.repository.hasOwnProperty('url')) {
     throw new Error('URL does not exist in repository section');
@@ -18,24 +17,24 @@ let pkg = JSON.parse(readFileSync("package.json"));
 } */
 
 let parsedUrl = url.parse(repoUrl);
-let repository = (parsedUrl.host || "") + (parsedUrl.path || "");
+let repository = (parsedUrl.host || '') + (parsedUrl.path || '');
 
-/* let ghToken = process.env.GH_TOKEN;
+let ghToken = process.env.GH_TOKEN;
 let npmUser = process.env.NPM_USER;
-let npmEmail = process.env.NPM_EMAIL; */
+let npmEmail = process.env.NPM_EMAIL;
 
-const npmUser = execSync("git config user.name").toString();
-const npmEmail = execSync("git config user.email").toString();
-cd(".doc");
-touch(".nojekyll");
-exec("git init");
-exec("git add .");
+const npmUser = execSync('git config user.name').toString();
+const npmEmail = execSync('git config user.email').toString();
+cd('docs-dist');
+touch('.nojekyll');
+exec('git init');
+exec('git add .');
 exec(`git config user.name "${npmUser}"`);
 exec(`git config user.email "${npmEmail}"`);
 exec('git commit -m "docs(docs): update gh-pages"');
-/* exec(
-  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
-); */
+exec(
+  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`,
+);
 // 因为没有在runner执行，本地自己输入账号密码
-exec(`git push --force --quiet "${repoUrl}" master:gh-pages`);
-echo("Docs deployed!!");
+// exec(`git push --force --quiet "${repoUrl}" master:gh-pages`);
+echo('Docs deployed!!');
